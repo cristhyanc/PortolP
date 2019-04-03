00053a5581714a665fd5d64bc3442134a008420c
Cristhyan Cardona <cristhyan@outlook.com>
2019-04-03 20:21:26 +1000

    Borderless
    Borderless

---
+++ Portol/PortolMobile.Forms/PortolMobile.Forms.Android/Effects/BorderlessEffect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+[assembly: ExportEffect(typeof(BorderlessEffect), "BorderlessEffect")]
+namespace PortolMobile.Forms.Droid.Effects
+{
+    class BorderlessEffect : PlatformEffect
+    {
+        Drawable oldBackground;
+        protected override void OnAttached()
+        {
+            if (Control == null)
+                return;
+
+            oldBackground = Control.Background;
+            Control.SetBackgroundResource(Droid.Resource.Drawable.EntryBottomBorder);
+        }
+
+        protected override void OnDetached()
+        {
+            if (Control == null)
+                return;
+
+            Control.Background = oldBackground;
+        }
+    }
+}
---
+++ Portol/PortolMobile.Forms/PortolMobile.Forms/Effects/BorderlessEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PortolMobile.Forms.Effects
+{
+    public class BorderlessEffect : RoutingEffect
+    {
+        public BorderlessEffect() : base("PortolMobileFormsEffects.BorderlessEffect") { }
+    }
+}
