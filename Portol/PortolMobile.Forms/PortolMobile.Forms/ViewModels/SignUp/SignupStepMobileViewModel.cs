51f22d6a5ba4079e3b9329c681aa96fc42b0df69
Cristhyan Cardona <cristhyan@outlook.com>
2019-04-07 17:34:07 +1000

    Deploy

--- Portol/PortolMobile.Forms/PortolMobile.Forms/ViewModels/SignUp/SignupStepMobileViewModel.cs
+++ Portol/PortolMobile.Forms/PortolMobile.Forms/ViewModels/SignUp/SignupStepMobileViewModel.cs
@@ -162,11 +162,11 @@ namespace PortolMobile.Forms.ViewModels.SignUp
                     return;
                 }
 
-                //var result = await _loginService.SendVerificationCode(user.PhoneNumber, user.PhoneCountryCode);
-                //if (result)
-                //{
+                var result = await _loginService.SendVerificationCode(user.PhoneNumber, user.PhoneCountryCode);
+                if (result)
+                {
                     await NavigationService.NavigateToAsync<SignupStepCodeViewModel>(user);
-               // }
+                }
             }
             catch (System.Exception ex)
             {
