b4304971078f0ad26b67ea1b59f4067f08c2ee7e
Cristhyan <cristhyan@outlook.com>
2019-06-08 17:53:12 +1000

    Maps
    iOS maps

--- Portol/PortolMobile.Forms/PortolMobile.Forms/Views/UserControls/AddressPickerView.xaml.cs
+++ Portol/PortolMobile.Forms/PortolMobile.Forms/Views/UserControls/AddressPickerView.xaml.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PortolMobile.Forms.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PortolMobile.Forms.Views.UserControls
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AddressPickerView : ContentPage
-	{
+	public partial class AddressPickerView : ExtendedContentPage
+    {
 		public AddressPickerView()
 		{
 			InitializeComponent ();
--- Portol/PortolMobile.Forms/PortolMobile.Forms/Views/UserControls/PicturePickerView.xaml.cs
+++ Portol/PortolMobile.Forms/PortolMobile.Forms/Views/UserControls/PicturePickerView.xaml.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PortolMobile.Forms.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PortolMobile.Forms.Views.UserControls
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PicturePickerView : ContentPage
-	{
+	public partial class PicturePickerView : ExtendedContentPage
+    {
 		public PicturePickerView ()
 		{
 			InitializeComponent ();
