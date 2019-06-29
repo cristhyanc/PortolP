e9a4508ee2ba0dbce85d584f2f3c594cd38180a3
Cristhyan Cardona <cristhyan@outlook.com>
2019-06-29 21:13:23 +1000

    profile Image

--- Portol/Portol.Common/DTO/PicturesDto.cs
+++ Portol/Portol.Common/DTO/PicturesDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Portol.Common.Helper;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Portol.Common.DTO
    public class PictureDto
     {
         public Guid PictureID { get; set; }
-        public string ImageUrl { get; set; }        
+        public string ImageUrl { get; set; } = Constants.BaseUrl + "/image/profile_avatar.png";
         public Guid ParentID { get; set; }
         public Byte[] ImageArray { get; set; }
 
