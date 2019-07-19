362f4007b08d1a79e953101f848be3f8fd836d47
Cristhyan <cristhyan@outlook.com>
2019-07-19 19:06:26 +1000

    Login Page styles

---
+++ Portol/PortolMobile.Forms/PortolMobile.Forms.iOS/CustomRenderer/ExtendedPageRender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using PortolMobile.Forms.Controls;
+using PortolMobile.Forms.iOS.CustomRenderer;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+/*
+[assembly: ExportRenderer(typeof(ExtendedContentPage), typeof(ExtendedPageRender))]
+namespace PortolMobile.Forms.iOS.CustomRenderer
+{
+    public class ExtendedPageRender : PageRenderer
+    {
+        private string backgroundurl = "";
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+
+            var page = e.NewElement as ExtendedContentPage;
+            backgroundurl = page.BackgroundImage;
+
+
+
+        }
+
+        
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(false);
+
+            if (!string.IsNullOrEmpty(backgroundurl))
+            {
+                UIGraphics.BeginImageContext(View.Bounds.Size);
+                UIImage i = UIImage.FromFile(backgroundurl);
+
+                if (i != null && View != null)
+                {
+                    i = i.Scale(View.Bounds.Size);
+                    if (i != null)
+                    {
+                        View.BackgroundColor = UIColor.FromPatternImage(i);
+                    }
+
+                }
+            }
+
+        }
+
+
+
+    }
+}*/
